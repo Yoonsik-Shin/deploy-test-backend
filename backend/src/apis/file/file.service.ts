6e876b8278e8372893d8f769b83a0c1d5a6f8ca7
Yoonsik-Shin <sys25800994@gmail.com>
2023-03-14 11:18:04 +0900

    버킷설정오류 업데이트

--- backend/src/apis/file/file.service.ts
+++ backend/src/apis/file/file.service.ts
@@ -3,25 +3,22 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class FileService {
   async upload({ files }) {
-    const waitedFiles = await Promise.all(files);
-    console.log(waitedFiles);
-
-    const storage = new Storage({
-      projectId: '프로젝트아이디',
-      keyFilename: '키파일이름',
-    }).bucket('폴더명');
-
-    const results = await Promise.all(
-      waitedFiles.map((el) => {
-        return new Promise((resolve, reject) => {
-          el.createReadStream()
-            .pipe(storage.file(el.filename).createWriteStream())
-            .on('finish', () => resolve('성공'))
-            .on('error', () => reject('실패'));
-        });
-      }),
-    );
-
-    return results;
+    // const waitedFiles = await Promise.all(files);
+    // console.log(waitedFiles);
+    // const storage = new Storage({
+    //   projectId: '프로젝트아이디',
+    //   keyFilename: '키파일이름',
+    // }).bucket('폴더명');
+    // const results = await Promise.all(
+    //   waitedFiles.map((el) => {
+    //     return new Promise((resolve, reject) => {
+    //       el.createReadStream()
+    //         .pipe(storage.file(el.filename).createWriteStream())
+    //         .on('finish', () => resolve('성공'))
+    //         .on('error', () => reject('실패'));
+    //     });
+    //   }),
+    // );
+    // return results;
   }
 }
